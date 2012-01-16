6c0e7ab48e7eb7cfa5617799e2b9ff622c78aebb
Juan Carlos Orozco <juanco89@gmail.com>
2012-01-16 18:17:16 -0500

    Primer commit.
    Primer commit, licencia y README.

---
+++ src/com/rockodroid/HomeActivity.java
@@ -0,0 +1,30 @@
+/*   
+    Rockodroid: Music Player for android
+    Copyright (C) 2012  Laura K. Salazar, Roberto R. De la Parra, Juan C. Orozco.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.rockodroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
